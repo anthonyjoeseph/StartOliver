749bf057fbe78959937fb8e9ff1f5d84d79da461
anthonygabriele <anthonyjoeseph@gmail.com>
2017-11-15 17:23:26 -0500

    added new slide

---
+++ src/App/Slides/Video/Video.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import Flexbox from 'flexbox-react';
+
+const styles = {
+  padded:{
+    textAlign: 'center',
+    paddingLeft: '235px',
+    paddingRight: '235px'
+  },
+  bigText:{
+    color: 'white',
+    fontFamily: "peachy-keen-jf",
+    fontWeight: '100',
+    fontSize: '30pt',
+    lineHeight: '125%'
+  },
+  smallText:{
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: "peachy-keen-jf",
+    fontWeight: '100',
+    fontSize: '15pt',
+    lineHeight: '125%'
+  }
+};
+
+class Video extends Component {
+  render() {
+    return (
+      <Flexbox
+        flexDirection="column"
+        justifyContent='space-around'
+        alignItems='center'
+        style={{height: '100%'}}
+      >
+        <Flexbox
+          style={{width:"100%"}}
+          flexDirection="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          flexGrow={2}
+        >
+          <div id="spacer" style={{width:'60px'}} />
+        </Flexbox>
+        <Flexbox
+          justifyContent="center"
+          alignItems="center"
+          flexGrow={1}
+        >
+          <div style={{...styles.padded, ...styles.bigText}}>
+            CENTERED
+          </div>
+        </Flexbox>
+        <Flexbox
+          style={{width:"100%",}}
+          flexDirection="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          flexGrow={2}
+        >
+          <div id="spacer" style={{width:'60px'}} />
+        </Flexbox>
+      </Flexbox>
+    );
+  }
+}
+
+export default Video;
